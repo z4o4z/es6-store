36040226bde2ee50be37caa1b3e39dd3984405c6
Evgeniy Zaitsev <overhawlin@gmail.com>
2016-06-24 15:20:49 +0300

    fix bugs

--- src/Store.js
+++ src/Store.js
@@ -1,71 +1,110 @@
-let __ls = Symbol('localStorage');
-let __store = Symbol('store');
-let __name = Symbol('name');
-let __isWriting = Symbol('isWriting');
+'use strict';
+
+const __ls = Symbol('localStorage');
+const __store = Symbol('store');
+const __window = Symbol('window');
+const __name = Symbol('name');
+const __isWriting = Symbol('isWriting');
+const __error = Symbol('error');
+
+function isWindowAndHasLS(window) {
+  return window && window.localStorage;
+}
+
+function isValidObject(object) {
+  return object && typeof object === "object" && !Array.isArray(object) && !(object instanceof Error);
+}
+
+function logError(error) {
+  console.error(error);
+}
+
+function error(message) {
+  return new Error(message);
+}
+
+function keyIsNotAString() {
+  return error("key should be a string");
+}
+
+function isKeyAString(key) {
+  return typeof key === "string";
+}
 
 export default class Store {
 
   /*
+   * from object to string
+   * @param {object}
+   */
+  static serialize(data) {
+    return JSON.stringify(data);
+  }
+
+  /*
+   * from string to object
+   * @param {string}
+   */
+  static deserialize(string) {
+    return JSON.parse(string);
+  }
+
+  /*
+   * cone object
+   * @param {obj}
+   */
+  static clone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+
+  /*
    * @param {string} name of local storage item
    */
-
   constructor(name) {
-    let store;
-
-    this.window = window;
-
-    if (!this.window || !this.window.localStorage) {
-      return Store.__logError(new Store.__error("localStorage is not defined!"));
+    if (!isWindowAndHasLS(window)) {
+      return logError(error("window or localStorage is not defined!"));
     }
 
+    this[__window] = window;
     this[__name] = name;
     this[__isWriting] = false;
-    this[__ls] = this.window.localStorage;
+    this[__error] = null;
+    this[__ls] = this[__window].localStorage;
+    this[__store] = this.__getAndDeserialize();
 
-    store = this.__getAndDeserialize();
-
-    if (store instanceof Error) {
-      return;
-    }
-
-    this[__store] = store;
-
-    if (this[__store] === null || typeof this[__store] !== "object" || Array.isArray(this[__store]) || this[__store] instanceof Error) {
+    if (!isValidObject(this[__store])) {
       this[__store] = {};
 
-      if (!this.__serializeAndSet()) {
-        Store.__logError(new Store.__error("Error when trying to set data to localStorage!"));
+      this.__serializeAndSet();
 
-        return;
+      if (this[__error]) {
+        this.destructor(true);
+
+        return this[__error];
       }
     }
 
     this.__changeStorageHandler = this.__changeStorageHandler.bind(this);
 
-    this.window.addEventListener("storage", this.__changeStorageHandler);
+    this[__window].addEventListener("storage", this.__changeStorageHandler);
   }
 
   /*
    * Remove event listener
    * @param {boolean} if true, local storage item will be removed
    */
-
   destructor(removeStorage) {
-    if (this.window === undefined) {
-      return;
-    }
-
-    this.window.removeEventListener("storage", this.__changeStorageHandler);
+    this[__window].removeEventListener("storage", this.__changeStorageHandler);
 
     if (removeStorage) {
       this[__ls].removeItem(this[__name]);
     }
 
-    this.window = null;
-    this[__name] = null;
-    this[__ls] = null;
-    this[__store] = null;
-    this[__isWriting] = false;
+    delete this[__window];
+    delete this[__name];
+    delete this[__ls];
+    delete this[__store];
+    delete this[__isWriting];
   }
 
   /*
@@ -73,40 +112,40 @@ export default class Store {
    * @param {string} value of key which you want set
    * @param {object|number|string|boolean} val which you want set
    */
-
   set(key, val) {
+    if (!isKeyAString(key)) {
+      return keyIsNotAString();
+    }
+
     let store = this[__store];
     let parts = key.split('.');
+    let lastKey = parts.pop();
     let _val = typeof val === "object" ? Store.clone(val) : val;
-
-    if (typeof key !== "string") {
-      return Store.__error("key should be a string");
-    }
 
     if (typeof val === "function") {
       _val = val();
     }
 
     if (_val === undefined) {
-      return this[__store].remove(key);
+      return this.remove(key);
     }
 
-    for (let i = 0, length = parts.length; i < length; i += 1) {
-      if (typeof store[parts[i]] === "undefined") {
-        store[parts[i]] = {};
+    parts.forEach(_key => {
+      if (!isValidObject(store[_key])) {
+        store[_key] = {};
       }
 
-      if (i === length - 1) {
-        store[parts[i]] = _val;
-      } else {
-        store = store[parts[i]];
-      }
-    }
+      store = store[_key];
+    });
+
+    store[lastKey] = _val;
 
     this[__isWriting] = true;
 
-    if (!this.__serializeAndSet()) {
-      return new Store.__error("Trying to set data in localStorage is failed!");
+    this.__serializeAndSet();
+
+    if (this[__error]) {
+      return this[__error];
     }
 
     return _val;
@@ -117,26 +156,30 @@ export default class Store {
    * @param {string} value of key which you want get
    * @param {object|number|string|boolean} defaultValue if key is undefined
    */
-
   get(key, defaultValue) {
+    if (!arguments.length) {
+      return this.getAll();
+    }
+
+    if (!isKeyAString(key)) {
+      return keyIsNotAString();
+    }
+
     let store = this[__store];
     let parts = key.split(".");
+    let lastKey = parts.pop();
 
-    if (typeof key !== "string") {
-      return Store.__error("key should be a string");
-    }
+    for (let i = 0; i < parts.length; i++) {
+      let _key = parts[i];
 
-    for (let val in parts) {
-      if (parts.hasOwnProperty(val)) {
-        let _store = store[val];
-
-        if (_store === undefined) {
-          return defaultValue;
-        }
-
-        store = _store;
+      if (store.hasOwnProperty(_key) && isValidObject(store[_key])) {
+        store = store[_key];
+      } else {
+        return defaultValue;
       }
     }
+
+    store = store[lastKey];
 
     if (store === undefined) {
       return defaultValue;
@@ -148,7 +191,6 @@ export default class Store {
   /*
    * return all local storage data
    */
-
   getAll() {
     return Store.clone(this[__store]);
   }
@@ -157,46 +199,75 @@ export default class Store {
    * you can use multikey: remove('boo.bar.baz')
    * @param {string} value of key which you want remove
    */
-
   remove(key) {
+    if (!arguments.length) {
+      return this.clear();
+    }
+
+    if (!isKeyAString()) {
+      return keyIsNotAString();
+    }
+
     let store = this[__store];
     let parts = key.split('.');
+    let lastKey = parts.pop();
     let val;
 
-    if (typeof key !== "string") {
-      return Store.__error("key should be a string");
-    }
+    for (let i = 0; i < parts.length; i++) {
+      let _key = parts[i];
 
-    for (let i = 0; i < parts.length; i += 1) {
-      if (typeof store[parts[i]] === "undefined") {
+      if (store.hasOwnProperty(_key) && isValidObject(store[_key])) {
+        store = store[_key];
+      } else {
         return;
       }
-
-      if (i === parts.length - 1) {
-        val = store[parts[i]];
-        delete store[parts[i]];
-      } else {
-        store = store[parts[i]];
-      }
     }
 
-    if (!this.__serializeAndSet()) {
-      return Store.__error("Trying to set data in localStorage is failed!");
+    val = store[lastKey];
+
+    delete store[lastKey];
+
+    this.__serializeAndSet();
+
+    if (this[__error]) {
+      return this[__error];
     }
 
     return val;
   }
 
+  /*
+   * Clears local storage.
+   */
+  clear() {
+    let store = this[__store];
+
+    this[__store] = {};
+    this.__serializeAndSet();
+
+    return store;
+  }
+
   __getAndDeserialize() {
-    return Store.deserialize(this[__ls].getItem(this[__name]));
+    try {
+      return Store.deserialize(this[__ls].getItem(this[__name]));
+    } catch (e) {
+      this[__error] = error("Error when trying to get data from localStorage!");
+
+      logError(this[__error]);
+
+      return this[__error];
+    }
   }
 
   __serializeAndSet() {
     try {
       this[__ls].setItem(this[__name], Store.serialize(this[__store]));
-      return true;
+      this[__error] = null;
     } catch (e) {
-      return false;
+      this[__error] = error("Error when trying to set data to localStorage!");
+
+      logError(this[__error]);
     }
   }
 
@@ -206,66 +277,12 @@ export default class Store {
       return;
     }
 
-    var store = this.__getAndDeserialize();
+    let store = this.__getAndDeserialize();
 
-    if (store instanceof Error) {
+    if (!isValidObject(store)) {
       return;
     }
 
     this[__store] = store;
-  }
-
-  /*
-   * Clears local storage.
-   */
-
-  clear() {
-    this.store = {};
-    this.__serializeAndSet();
-  }
-
-  /*
-   * from object to string
-   * @param {object}
-   */
-
-  static serialize(data) {
-    return JSON.stringify(data);
-  }
-
-  /*
-   * from string to object
-   * @param {string}
-   */
-
-  static deserialize(string) {
-    if (typeof string !== "string") {
-      return;
-    }
-
-    // try to parse string
-    try {
-      return JSON.parse(string);
-    } catch (e) {
-      Store.__logError(e);
-      return e;
-    }
-  }
-
-  /*
-   * cone object
-   * @param {obj}
-   */
-
-  static clone(obj) {
-    return JSON.parse(JSON.stringify(obj));
-  }
-
-  static __logError(error) {
-    console.error(error);
-  }
-
-  static __error(message) {
-    return new Error(message);
   }
 }
